f89d608e070e5c7aedf2062ca6390a846e426dfd
Ivan <ifrancesalcantara@gmail.com>
2019-12-19 13:17:19 +0100

    loading profile

--- src/components/ProfileDisplay/ProfileDisplay.js
+++ src/components/ProfileDisplay/ProfileDisplay.js
@@ -65,10 +65,10 @@ export default class ProfileDisplay extends Component {
               </span>
             )}
 
-            {!this.state.user.paintings ? <h1>{this.state.user.username} has no style cards</h1> : !this.state.user
+            {!this.state.user.paintings ? <LoadingGif/> : !this.state.user
                 .paintings[0] ? <h1 className="profile-empty-err">You don't have any style cards yet :(</h1> : (
               <div>
-                {!this.props.user ? <h1 id="details-title-not-user">{this.state.user.username}'s paintings</h1> : this.props.user._id !== this.state.user._id ? (
+                {!this.props.user ? <h1 id="details-title-not-user">{this.state.user.username}'s style cards</h1> : this.props.user._id !== this.state.user._id ? (
                   <h1>{this.state.user.username}'s style cards</h1>
                 ) : (
                   <h1>Your style cards</h1>
