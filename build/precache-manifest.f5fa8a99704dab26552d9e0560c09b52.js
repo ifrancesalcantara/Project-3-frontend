df1e3a6a91b092d3c32260fa25366fcc2279ee4d
Ivan <ifrancesalcantara@gmail.com>
2019-12-23 17:50:30 +0100

    resetted to solve conflict

---
+++ build/precache-manifest.f5fa8a99704dab26552d9e0560c09b52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7cdb959e1ff34408c119f3b4505352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf1d4e3bb683caf106d",
+    "url": "/static/css/main.80543b5f.chunk.css"
+  },
+  {
+    "revision": "9f7a29005a4174bbb57a",
+    "url": "/static/js/2.548d019b.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.548d019b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bf1d4e3bb683caf106d",
+    "url": "/static/js/main.1eb20142.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  }
+]);
