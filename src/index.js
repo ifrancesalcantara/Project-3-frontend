797ed6efad8d21833ca869a76cce97d3050f2e0e
Ivan <ifrancesalcantara@gmail.com>
2019-12-23 20:02:43 +0100

    compact sorting function in Home view

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+import BabelCore from "babel-core"
 
 import App from "./App";
 import AuthProvider from "./lib/AuthProvider";
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -21,69 +21,56 @@ class Home extends React.Component {
 
   //!!!DELETE BACKEND / part
   sortPaintings = async sort => {
-    if(sort===this.state.lastSearch) { return }
-    let sortedPaintings;
-
-    if(sort ==="Newest") {
-      sortedPaintings = this.state.paintings.sort((a,b)=>{
-        if(a.created_at>b.created_at){
-          return -1
-        } else {
-          return 1
-        }
-      })
-
-    } else if(sort ==="Oldest") {
-      sortedPaintings = this.state.paintings.sort((a,b)=>{
-        if(a.created_at>b.created_at){
-          return 1
-        } else {
-          return -1
-        }
-      })
-
-    } else if(sort ==="Most liked") {
-      sortedPaintings = this.state.paintings.sort((a,b)=>{
-        if(a.usersWhoLiked.length>b.usersWhoLiked.length){
-          return -1
-        } else {
-          return 1
-        }
-      })
-
-    } else if(sort ==="Most seen") {
-      sortedPaintings = this.state.paintings.sort((a,b)=>{
-        if(a.timesSeen>b.timesSeen){
-          return -1
-        } else {
-          return 1
-        }
-      })
+    if (sort === this.state.lastSearch) {
+      return;
     }
 
-    this.setState({displayedPaintings: sortedPaintings, lastSearch: sort})
+    let sortedPaintings;
+
+    if (sort === "Oldest") {
+      sortedPaintings = this.state.paintings.sort((a, b) => {
+        return a.created_at > b.created_at ? 1 : -1;
+      });
+    } else if (sort === "Newest") {
+      sortedPaintings = this.state.paintings.sort((a, b) => {
+        return a.created_at > b.created_at ? -1 : 1;
+      });
+    } else if (sort === "Most liked") {
+      sortedPaintings = this.state.paintings.sort((a, b) => {
+        return a.usersWhoLiked.length > b.usersWhoLiked.length ? -1 : 1;
+      });
+    } else if (sort === "Most seen") {
+      sortedPaintings = this.state.paintings.sort((a, b) => {
+        return a.timesSeen > b.timesSeen ? -1 : 1;
+      });
+    }
+    this.setState({ displayedPaintings: sortedPaintings, lastSearch: sort });
   };
 
-  filterPaintings=(filter)=>{
-    this.setState({displayedPaintings: this.state.paintings.filter(painting=>{
-      return painting.title.includes(filter)
-    })})
-  }
+  filterPaintings = filter => {
+    this.setState({
+      displayedPaintings: this.state.paintings.filter(painting => {
+        return painting.title.toLowerCase().includes(filter.toLowerCase());
+      })
+    });
+  };
 
   componentDidMount = async () => {
     const homePaintings = await paintingService.getHomePaintings();
-    this.setState({ paintings: homePaintings, displayedPaintings: homePaintings })
-  }
+    this.setState({
+      paintings: homePaintings,
+      displayedPaintings: homePaintings
+    });
+  };
 
-  handleLike =(string)=>{
-    userService.handleLike(string)
-  }
-  
+  handleLike = string => {
+    userService.handleLike(string);
+  };
+
   render() {
     const { user, isLoggedIn } = this.props;
     return (
       <div>
-
         <Navbar view="home" getHomePaintings={this.getHomePaintings} />
         <ExploreBar
           {...this.props}
@@ -92,22 +79,27 @@ class Home extends React.Component {
         />
 
         {!this.state.paintings ? null : (
-          <PaintingList user={user} paintings={this.state.displayedPaintings} 
-          handleLike={this.handleLike}/>
+          <PaintingList
+            user={user}
+            paintings={this.state.displayedPaintings}
+            handleLike={this.handleLike}
+          />
         )}
 
         {isLoggedIn ? (
           <div>
-            
-          <Link to={`/chats/${user._id}`} className="chats-link">
-          <img src="https://img.icons8.com/flat_round/64/000000/speech-bubble.png" alt=""/>
-          </Link>
-          <Link to="/paintings/add" className="add-link">
-            <img
-              src="https://img.icons8.com/material/96/000000/add.png"
-              alt=""
-            />
-          </Link>
+            <Link to={`/chats/${user._id}`} className="chats-link">
+              <img
+                src="https://img.icons8.com/flat_round/64/000000/speech-bubble.png"
+                alt=""
+              />
+            </Link>
+            <Link to="/paintings/add" className="add-link">
+              <img
+                src="https://img.icons8.com/material/96/000000/add.png"
+                alt=""
+              />
+            </Link>
           </div>
         ) : null}
       </div>
